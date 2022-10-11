4eda5b5dc21a4e743780d3cfd1bae447a866f3b8
imaiyuichi <yu.imaiyu4@gmail.com>
2022-10-11 23:44:17 +0900

    length、substring()、文字へのアクセス方法

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
 'use strict';
 {
-    // let x = 1;
-    // let y = x;
-    // x = 5;
-    // console.log(x);  //1
-    // console.log(y);  //5
+    // lengthを使用すると、文字数を取得する
+    const str = 'hello';
+    console.log(str.length);
 
-    // let x = [1, 2];
-    // let y = x;
-    // x[0] = 5;
-    // console.log(x);  //5,2
-    // console.log(y);  //5,2
+    // 指定した場所の文字列を取得する。
+    // atr.substring(開始位置,終了位置)
+    console.log(str.substring(2,4));
 
-    // 配列を書き換えた場合は、配列の場所を記憶し探しにいく。
-    // そのため、xの配列を書き換えると、yはxの場所を探しxの値を出力する。
-
-    // スプレット演算子
-    // 配列として記憶させたい場合はスプレット演算子を使う。
-    let x = [1, 2];
-    let y = [...x];
-    x[0] = 5;
-    console.log(x);  //5,2
-    console.log(y);  //1,2
+    // 配列のように記述する事で、個々の文字に対してアクセルする事ができる。
+    // ただ値の取得をしている訳ではないので、forEach()など使える訳ではない。
+    console.log(str[1]);
 }
