dad9f57eca7e82b4836a41ed0750c3f2aad065fb
imaiyuichi <yu.imaiyu4@gmail.com>
2022-10-11 23:53:14 +0900

    join()、split()

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 'use strict';
 {
-    // lengthを使用すると、文字数を取得する
-    const str = 'hello';
-    console.log(str.length);
+    // join()
+    // 複数の文字列に対して、結合して"/"をつける
+    const d = [2019, 11 , 14];
+    console.log(d.join('/'));
+    console.log(d.join(''));
 
-    // 指定した場所の文字列を取得する。
-    // atr.substring(開始位置,終了位置)
-    console.log(str.substring(2,4));
+    // split()
+    // 文字列を分割する
+    const t = '17:08:24';
+    console.log(t.split(':'));
 
-    // 配列のように記述する事で、個々の文字に対してアクセルする事ができる。
-    // ただ値の取得をしている訳ではないので、forEach()など使える訳ではない。
-    console.log(str[1]);
+    // 分割代入を使用し、返り値を別々の定数に代入する
+    const [hour, minute, second] = t.split(':');
+    console.log(hour);
+    console.log(minute);
+    console.log(second);
 }
