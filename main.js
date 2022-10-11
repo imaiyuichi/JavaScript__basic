f373e6a91f8a15b169ea46dd87ffec29b671ef37
imaiyuichi <yu.imaiyu4@gmail.com>
2022-10-12 00:09:18 +0900

    数値の操作、Math.floor()、Math.ceil()、Math.round()、toFixed()、Math.random()

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 'use strict';
 {
-    // join()
-    // 複数の文字列に対して、結合して"/"をつける
-    const d = [2019, 11 , 14];
-    console.log(d.join('/'));
-    console.log(d.join(''));
+    const scores = [10, 6, 8];
 
-    // split()
-    // 文字列を分割する
-    const t = '17:08:24';
-    console.log(t.split(':'));
+    let sum = 0;
 
-    // 分割代入を使用し、返り値を別々の定数に代入する
-    const [hour, minute, second] = t.split(':');
-    console.log(hour);
-    console.log(minute);
-    console.log(second);
+    // forEachを使用し、scoresの配列ないを計算する
+    scores.forEach(score => {
+        sum += score;
+    });
+    
+    // length = 文字数
+    const avg = sum / scores.length;
+    console.log(sum);
+    console.log(avg);
 }
