66db1cbd8e471ac35c8e60467e796a5093e16155
imaiyuichi <yu.imaiyu4@gmail.com>
2022-10-23 22:50:48 +0900

    複数のオブジェクト 関数による処理

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 'use strict';
 {
-    // const name = 'imai';
-    // // toUpperCase = 小文字を大文字に変更する。文字列にのみ使用できる。
-    // console.log(name.toUpperCase());
-    // console.log('finish')
-
-    // プログラムはエラーが発生してしまうと、それ以降の処理が停止してしまう。
-    // 開発者が想定していなかった理由で起きるエラーを例外という。
-    // 例外が発生しても、処理を止めたくない場合はtyr{}で囲ってあげる
-
-    const name = 3;
-    // toUpperCase = 小文字を大文字に変更する。文字列にのみ使用できる。
-    // 今回は使用者が誤って文字列に数字を入れてしまった場合
-    try {
-        console.log(name.toUpperCase());
-    // catchは引数を取ることができる
-    } catch (e) {
-        console.log(e);
-    }
-    console.log('finish');
+    const post = [
+        {
+            text: 'JavaScriptの勉強中・・・',
+            likeCount: 0,
+            show() {
+                console.log(`${this.text} - ${this.likeCount}いいね`);
+            }  
+        },
+        {
+            text:'プログラミング楽しい',
+            likeCount: 0,
+            show() {
+                console.log(`${this.text} - ${this.likeCount}いいね`);
+            }  
+        },
+    ];
+    post[0].show();
+    post[1].show();
 }
