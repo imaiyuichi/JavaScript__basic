1a32d90e5181d06ded6fda5840ca3d773ad276c7
imaiyuichi <yu.imaiyu4@gmail.com>
2022-10-23 23:05:57 +0900

    class constructor() this new

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 'use strict';
 {
-    const post = [
-        {
-            text: 'JavaScriptの勉強中・・・',
-            likeCount: 0,
-            show() {
-                console.log(`${this.text} - ${this.likeCount}いいね`);
-            }  
-        },
-        {
-            text:'プログラミング楽しい',
-            likeCount: 0,
-            show() {
-                console.log(`${this.text} - ${this.likeCount}いいね`);
-            }  
-        },
-    ];
-    post[0].show();
-    post[1].show();
+    // クラス名のPostの先頭の文字は慣例的に大文字にする事が多い
+    class Post {
+        // constructorは引数を取る事がでいる
+        constructor(text) {
+            this.text = text;
+            this.likeCount = 0;
+        }
+        show() {
+            console.log(`${this.text} - ${this.likeCount}いいね`);
+        }
+    }
+
+    const posts = [
+        new Post('JavaScript勉強中'),
+        new Post('プログラミング楽しい'),
+    ]
+    posts[0].show();
+    posts[1].show();
+    
 }
