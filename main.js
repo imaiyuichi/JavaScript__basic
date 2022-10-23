9c415957791108bbe1a0016f6cbea7e8bf5041b6
imaiyuichi <yu.imaiyu4@gmail.com>
2022-10-23 23:20:58 +0900

    インスタンスを介さずに直接クラスから呼び出せる、静的メソッドの使い方 静的メソッド static

--- main.js
+++ main.js
@@ -15,6 +15,12 @@
             // 同じクラス内のメソッドはthisを使用して呼び正す事ができる
             this.show();
         }
+
+        // 静的メソッド
+        // インスタンスを作られず呼び出すため、メソット内にthisは使用できない
+        static showInfo() {
+            console.log('Post class version 1.0');
+        }
     }
 
     const posts = [
@@ -22,8 +28,10 @@
         new Post('プログラミング楽しい'),
     ]
 
-    posts[0].like();
+    // posts[0].like();
     // posts[0].show();
     // posts[1].show();
+
+    Post.showInfo();
     
 }
